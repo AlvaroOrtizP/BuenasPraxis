55850d7f850170fc03e92e5429251bc97ee4fe61
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2023-01-08 16:40:57 +0100

    Creaccion de proyecto

---
+++ src/com/praxis/concadenar/Concadenar.java
@@ -0,0 +1,16 @@
+package com.praxis.concadenar;
+
+public class Concadenar {
+
+	public static void main(String[] args) {
+		//Para concadenar se puede usar 
+		// + 		: Crea una nueva instancia de texto por lo cual usa mas memoria
+		// concat	: Añade en la actual cadena mas texto por lo que usa la memoria ya asignada
+		
+		//La mejor opcion seria conca
+		
+		System.out.println("Texto ".concat("Algo"));
+
+	}
+
+}
